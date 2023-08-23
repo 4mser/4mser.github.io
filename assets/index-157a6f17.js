a6830bf3239511d3e445e80577eb83ebf2aa7d30
4mser <nicolas.moreno.sagan@gmail.com>
2023-08-23 15:29:17 -0400

    Updates

--- assets/index-157a6f17.js
+++ assets/index-157a6f17.js
@@ -380,7 +380,7 @@ Error generating stack: `+o.message+`
   justify-content: space-between;
   flex-direction: column;
   padding: 10px;
-`,ox={Playas:[{id:1,nombre:"Niebla",imagen:"https://app-valdi.s3.amazonaws.com/niebla.jpg"},{id:2,nombre:"Los Molinos",imagen:"https://app-valdi.s3.amazonaws.com/losmolinos.jpg"},{id:3,nombre:"San Ignacio",imagen:"https://app-valdi.s3.amazonaws.com/sanignacio.jpg"},{id:4,nombre:"Playa Rosada",imagen:"https://app-valdi.s3.amazonaws.com/playarosada.jpg"},{id:5,nombre:"Loncoyen",imagen:"https://th.bing.com/th/id/OIP.9TgEnSlRvOZw4tFFb4gjKAHaE8?pid=ImgDet&rs=1"},{id:6,nombre:"Centenilla",imagen:"https://i.pinimg.com/originals/8e/ab/01/8eab01a6fde8f7f7339e4879ea6ec6d9.jpg"},{id:7,nombre:"La Misión",imagen:"https://i.pinimg.com/736x/1d/ae/6d/1dae6dffed5c6f0838121446905e3d4e--romantic.jpg"},{id:8,nombre:"Calfuco",imagen:"https://th.bing.com/th/id/R.1f0906613f2ea59f398b7074850d44b3?rik=8TelC2U%2b4e7gnw&riu=http%3a%2f%2fwww.calfuco.cl%2f2017%2fwp-content%2fuploads%2fsites%2f2685%2f2016%2f07%2fVista-1-playa3-1024x461.jpg&ehk=ZQaCGKNLapYdBc%2brGi4hZHbybIs7oI59Vf%2fboMbi2TI%3d&risl=&pid=ImgRaw&r=0"},{id:9,nombre:"Curiñanco",imagen:"https://th.bing.com/th/id/R.15afe66ca41d3da9c91d5a4c8619ad71?rik=Qv554G6vYDI3lA&pid=ImgRaw&r=0&sres=1&sresct=1"},{id:10,nombre:"Pilolcura",imagen:"https://i.pinimg.com/736x/f6/15/af/f615af7160ef19a37c0a467cb830fc0c.jpg"}],Parques:[{id:1,nombre:"Parque Oncol",imagen:"https://app-valdi.s3.amazonaws.com/parqueoncol.jpg"},{id:2,nombre:"Parque Saval",imagen:"https://www.rioenlinea.cl/wp-content/uploads/2021/07/parque_saval_municipio.jpeg"},{id:3,nombre:"Parque Nacional Alerce Costero",imagen:"https://storage.googleapis.com/chile-travel-static-content/2021/05/abuelo-alerce-milenario-en-parque-nacional-alerce-costero-1.jpg"},{id:4,nombre:"Arboretum",imagen:"https://app-valdi.s3.amazonaws.com/arboretum2.jpg"}],Reservas:[{id:1,nombre:"Reserva Punta Curiñanco",imagen:"https://app-valdi.s3.amazonaws.com/curi%C3%B1anco.jpg"}],Ríos:[{id:1,nombre:"Río Calle Calle",imagen:"https://app-valdi.s3.amazonaws.com/callecalle.jpg"}],Lagos:[{id:1,nombre:"Lago Ranco",imagen:"https://th.bing.com/th/id/R.c4698ec4e65f984aaf3d42f378946c9c?rik=hC3gDMPlMypdUQ&riu=http%3a%2f%2fwww.fmdos.cl%2fwp-content%2fuploads%2f2015%2f02%2fcascadas-lago-ranco.jpg&ehk=66hk%2bzoDpBDOCqkk4TF2C92lr7FYkKGr9H3SnVriGFc%3d&risl=&pid=ImgRaw&r=0"}],Islas:[{id:1,nombre:"Isla Mancera",imagen:"https://live.staticflickr.com/4680/38274087254_ca6e5d4bbf_b.jpg"},{id:2,nombre:"Isla del Rey",imagen:"https://media-cdn.tripadvisor.com/media/photo-s/1a/5f/da/85/isla-del-rey.jpg"}]},ax=({tema:e,activeCategory:t})=>{const n=ox[t]||[];return A.jsx(sx,{tema:e,children:A.jsx(Pn,{tema:e,centeredSlides:!1,spaceBetween:15,slidesPerView:2,freeMode:!0,navigation:!0,className:"mySwiper",children:n.map(r=>A.jsx(ut,{className:"slider",tema:e,children:A.jsxs(lx,{tema:e,children:[A.jsx("p",{children:r.nombre}),A.jsx("div",{className:"degree"}),A.jsx("img",{src:r.imagen,alt:r.nombre})]})},r.id))})})},sx=W.div`
+`,ox={Playas:[{id:1,nombre:"Niebla",imagen:"https://app-valdi.s3.amazonaws.com/niebla.jpg"},{id:2,nombre:"Los Molinos",imagen:"https://app-valdi.s3.amazonaws.com/losmolinos.jpg"},{id:3,nombre:"San Ignacio",imagen:"https://app-valdi.s3.amazonaws.com/sanignacio.jpg"},{id:4,nombre:"Playa Rosada",imagen:"https://app-valdi.s3.amazonaws.com/playarosada.jpg"},{id:5,nombre:"Loncoyen",imagen:"https://th.bing.com/th/id/OIP.9TgEnSlRvOZw4tFFb4gjKAHaE8?pid=ImgDet&rs=1"},{id:6,nombre:"Centenilla",imagen:"https://i.pinimg.com/originals/8e/ab/01/8eab01a6fde8f7f7339e4879ea6ec6d9.jpg"},{id:7,nombre:"La Misión",imagen:"https://i.pinimg.com/736x/1d/ae/6d/1dae6dffed5c6f0838121446905e3d4e--romantic.jpg"},{id:8,nombre:"Calfuco",imagen:"https://th.bing.com/th/id/R.1f0906613f2ea59f398b7074850d44b3?rik=8TelC2U%2b4e7gnw&riu=http%3a%2f%2fwww.calfuco.cl%2f2017%2fwp-content%2fuploads%2fsites%2f2685%2f2016%2f07%2fVista-1-playa3-1024x461.jpg&ehk=ZQaCGKNLapYdBc%2brGi4hZHbybIs7oI59Vf%2fboMbi2TI%3d&risl=&pid=ImgRaw&r=0"},{id:9,nombre:"Curiñanco",imagen:"https://th.bing.com/th/id/R.15afe66ca41d3da9c91d5a4c8619ad71?rik=Qv554G6vYDI3lA&pid=ImgRaw&r=0&sres=1&sresct=1"},{id:10,nombre:"Pilolcura",imagen:"https://i.pinimg.com/736x/f6/15/af/f615af7160ef19a37c0a467cb830fc0c.jpg"}],Parques:[{id:1,nombre:"Parque Oncol",imagen:"https://app-valdi.s3.amazonaws.com/parqueoncol.jpg"},{id:2,nombre:"Parque Saval",imagen:"https://www.rioenlinea.cl/wp-content/uploads/2021/07/parque_saval_municipio.jpeg"},{id:3,nombre:"Parque Nacional Alerce Costero",imagen:"https://storage.googleapis.com/chile-travel-static-content/2021/05/abuelo-alerce-milenario-en-parque-nacional-alerce-costero-1.jpg"},{id:4,nombre:"Arboretum",imagen:"https://app-valdi.s3.amazonaws.com/arboretum2.jpg"}],Reservas:[{id:1,nombre:"Reserva Punta Curiñanco",imagen:"https://app-valdi.s3.amazonaws.com/curi%C3%B1anco.jpg"}],Ríos:[{id:1,nombre:"Río Calle Calle",imagen:"https://app-valdi.s3.amazonaws.com/callecalle.jpg"}],Lagos:[{id:1,nombre:"Lago Ranco",imagen:"https://th.bing.com/th/id/R.c4698ec4e65f984aaf3d42f378946c9c?rik=hC3gDMPlMypdUQ&riu=http%3a%2f%2fwww.fmdos.cl%2fwp-content%2fuploads%2f2015%2f02%2fcascadas-lago-ranco.jpg&ehk=66hk%2bzoDpBDOCqkk4TF2C92lr7FYkKGr9H3SnVriGFc%3d&risl=&pid=ImgRaw&r=0"}],Islas:[{id:1,nombre:"Isla Mancera",imagen:"https://live.staticflickr.com/4680/38274087254_ca6e5d4bbf_b.jpg"},{id:2,nombre:"Isla del Rey",imagen:"https://media-cdn.tripadvisor.com/media/photo-s/1a/5f/da/85/isla-del-rey.jpg"}]},ax=({tema:e,activeCategory:t})=>{const n=ox[t]||[];return A.jsx(sx,{tema:e,children:A.jsx(Pn,{tema:e,centeredSlides:!1,spaceBetween:15,slidesPerView:2.1,freeMode:!0,navigation:!0,className:"mySwiper",children:n.map(r=>A.jsx(ut,{className:"slider",tema:e,children:A.jsxs(lx,{tema:e,children:[A.jsx("img",{src:r.imagen,alt:r.nombre}),A.jsx("p",{children:r.nombre})]})},r.id))})})},sx=W.div`
   width: 100%;
   user-select: none;
   .mySwiper {
@@ -389,11 +389,14 @@ Error generating stack: `+o.message+`
   }
 
   .slider {
-    border-radius: 10px;
+    border-radius: 5px;
     overflow: hidden;
     user-select: none;
     box-shadow: 0 3px 7px -3px ${e=>e.tema==="dark"?"black":"var(--shadow)"};
+    background: ${e=>e.tema==="dark"?"var(--Item)":"var(--cardsLight)"};
+    /* border: 1px solid #dddddd4a; */
     cursor: pointer;
+    background: ${e=>e.tema==="dark"?"":"var(--cardsLight)"};
   }
 
   h2 {
@@ -412,23 +415,22 @@ Error generating stack: `+o.message+`
   justify-content: space-between;
   align-items: center;
   flex-direction: column;
-  box-shadow: 0 1px 7px -3px ${e=>e.tema==="dark"?"var(--boxShadow)":"var(--shadow)"};
 
   p {
     width: 100%;
-    font-size: 0.7rem;
+    font-size: 10px;
     font-weight: 400;
-    position: absolute;
-    padding: 0 10px;
+    padding: 10px;
     bottom: 5px;
     z-index: 2;
-    color: var(--whiteColor);
+    color: ${e=>e.tema==="dark"?"var(--whiteColor)":"var(--Item)"};
   }
 
   img {
     width: 100%;
-    height: 9rem;
+    height: 12rem;
     transition: 0.3s ease-in-out;
+    /* border-bottom: 1px solid #dddddd4a; */
     object-fit: cover;
   }
 
