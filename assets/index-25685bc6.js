98f2998ed764e67d702e426d6f7c221f5d4a6708
4mser <nicolas.moreno.sagan@gmail.com>
2023-08-25 18:54:02 -0400

    Updates

--- assets/index-25685bc6.js
+++ assets/index-25685bc6.js
@@ -947,7 +947,7 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
   position: fixed;
   bottom: 0;
   /* border-top: 1px solid var(--borderDark); */
-  height: 4rem;
+  height: 4.5rem;
   box-shadow: 0 -1px 10px -7px var(--boxShadow);
   display: flex;
   align-items: center;
